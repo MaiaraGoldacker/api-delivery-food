1a87b2752d521f7a93afa7b9387b90599162a3fa
Maiara Goldacker <maiaragoldacker@gmail.com>
2020-09-08 10:21:29 -0300

    busca por id, suporte de formato xml

--- src/main/java/com/api/algafood/domain/model/Cozinha.java
+++ src/main/java/com/api/algafood/domain/model/Cozinha.java
@@ -1,14 +1,18 @@
 package com.api.algafood.domain.model;
 
 import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
+@JsonRootName("gastronomia") //muda o nome do root quando retornar em formato xml
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -19,6 +23,8 @@ public class Cozinha {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
+	//@JsonIgnore //remover propriedade da representação. Não aparecerá no retorno
+	@JsonProperty("titulo") //mudar o nome na representação do payload desse objeto 
 	@Column(nullable = false)
 	private String nome;
 
