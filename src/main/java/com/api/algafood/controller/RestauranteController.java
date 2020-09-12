f02d4b2cb176922670b293685da50299531d10fb
Maiara Goldacker <maiaragoldacker@gmail.com>
2020-09-12 13:32:55 -0300

    endpoint alterar restaurante -desafio

--- src/main/java/com/api/algafood/controller/RestauranteController.java
+++ src/main/java/com/api/algafood/controller/RestauranteController.java
@@ -2,19 +2,18 @@ package com.api.algafood.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.api.algafood.domain.exception.EntidadeNaoEncontradaException;
-import com.api.algafood.domain.model.Cozinha;
 import com.api.algafood.domain.model.Restaurante;
 import com.api.algafood.domain.repository.RestauranteRepository;
 import com.api.algafood.domain.service.CadastroRestauranteService;
@@ -43,8 +42,24 @@ public class RestauranteController {
 		return ResponseEntity.notFound().build();
 	}
 	
+	
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante){
+		try {
+			var restauranteAtual = restauranteRepository.buscar(restauranteId);
+		
+			if(restauranteAtual != null) {
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+				cadastroRestauranteService.salvar(restauranteAtual);
+				return ResponseEntity.ok(restauranteAtual);
+			}
+			return ResponseEntity.notFound().build();
+		}catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
 	@PostMapping
-
 	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
 		try {
 			restaurante = cadastroRestauranteService.salvar(restaurante);
