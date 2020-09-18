1221c07da6730fcb9a2164d681c5db3d48550293
Maiara Goldacker <maiaragoldacker@gmail.com>
2020-09-18 09:53:05 -0300

    consulta dinamica

--- src/main/java/com/api/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/api/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,12 +1,15 @@
 package com.api.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.api.algafood.domain.model.Restaurante;
 import com.api.algafood.domain.repository.RestauranteRepositoryQueries;
@@ -20,13 +23,30 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
 	
-		var jpql = "from Restaurante where nome like :nome "
-				+ "and taxaFrete between :taxaFreteInicial and :taxaFreteFinal";
+		var jpql = new StringBuilder();
 		
-		return manager.createQuery(jpql, Restaurante.class)
-				.setParameter("nome", "%" + nome + "%")
-				.setParameter("taxaFreteInicial", taxaFreteInicial)
-				.setParameter("taxaFreteFinal", taxaFreteFinal)
-				.getResultList();
+		var parametros = new HashMap<String, Object>();
+		
+		jpql.append("from Restaurante where 0=0 ");
+		
+		if (StringUtils.hasLength(nome)) {
+			jpql.append("and nome like :nome ");
+			parametros.put("nome", "%"+ nome + "%");
+		}
+		
+		if (taxaFreteInicial != null) {
+			jpql.append("and taxaFrete >= :taxaFreteInicial ");
+			parametros.put("taxaFreteInicial", taxaFreteInicial);
+		}
+		
+		if (taxaFreteFinal != null) {
+			jpql.append("and taxaFrete <= :taxaFreteFinal ");
+			parametros.put("taxaFreteFinal", taxaFreteFinal);
+		}		
+		
+		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
+		parametros.forEach((chave, valor)-> query.setParameter(chave, valor));
+		
+		return query.getResultList();			
 	}
 }
