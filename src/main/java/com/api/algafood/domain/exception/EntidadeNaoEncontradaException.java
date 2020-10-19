3511794d418a04bad50b341b0ae0796d946e6d9c
Maiara Goldacker <maiaragoldacker@gmail.com>
2020-10-19 12:47:29 -0300

    exception

--- src/main/java/com/api/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/api/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -2,13 +2,18 @@ package com.api.algafood.domain.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND/*, reason = "Entidade não encontrada"*/) //toda a vez que chamar essa exception, o retorno do status HTTP será notfound, e o reason serve para alterar message do erro será entidade não encontrada.
-public class EntidadeNaoEncontradaException extends RuntimeException{
+//@ResponseStatus(value = HttpStatus.NOT_FOUND/*, reason = "Entidade não encontrada"*/) //toda a vez que chamar essa exception, o retorno do status HTTP será notfound, e o reason serve para alterar message do erro será entidade não encontrada.
+public class EntidadeNaoEncontradaException extends ResponseStatusException{
 
 private static final long serialVersionUID = 1L;
 	
+	public EntidadeNaoEncontradaException(HttpStatus status, String mensagem) {
+		super(status, mensagem);
+	}
+
 	public EntidadeNaoEncontradaException(String mensagem) {
-		super(mensagem);
+		this(HttpStatus.NOT_FOUND, mensagem);
 	}
 }
