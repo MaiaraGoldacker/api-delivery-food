76c9092ad550cb0cb261efd806d20431d50523f1
Maiara Goldacker <maiaragoldacker@gmail.com>
2020-11-02 10:36:35 -0300

    padronizando erros de exception

--- src/main/java/com/api/algafood/controller/RestauranteController.java
+++ src/main/java/com/api/algafood/controller/RestauranteController.java
@@ -1,14 +1,23 @@
 package com.api.algafood.controller;
 
+import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,6 +30,8 @@ import com.api.algafood.domain.exception.NegocioException;
 import com.api.algafood.domain.model.Restaurante;
 import com.api.algafood.domain.repository.RestauranteRepository;
 import com.api.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @RestController
@@ -58,33 +69,43 @@ public class RestauranteController {
 		}
 	}
 	
-	/*@PatchMapping("/{restauranteId}")
+	@PatchMapping("/{restauranteId}")
 	public Restaurante atualizarParcial(@PathVariable Long restauranteId, 
-			@RequestBody Map<String, Object> campos){
-		var restauranteAtual = restauranteRepository.findById(restauranteId);
+			@RequestBody Map<String, Object> campos, HttpServletRequest request){
+		Restaurante restauranteAtual = cadastroRestauranteService.buscarOuFalhar(restauranteId);
 		
-		if(restauranteAtual.isEmpty()) {
-			return null;
-			//return ResponseEntity.notFound().build();
-		}
-	
-		merge(campos, restauranteAtual.get());
-		return atualizar(restauranteId, restauranteAtual.get());
-	
+		merge(campos, restauranteAtual, request);
+		return atualizar(restauranteId, restauranteAtual);
 	}
 
-	private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
-		ObjectMapper objectMapper = new ObjectMapper();
-		Restaurante restauranteOrigem = objectMapper.convertValue(camposOrigem, Restaurante.class);
+	private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
 		
-		camposOrigem.forEach((nomeProp, valorProp) ->{
-			Field field = ReflectionUtils.findField(Restaurante.class, nomeProp);
-			field.setAccessible(true); //muda tipo de acesso das propriedades- private para public
+		ServletServerHttpRequest servletServerHttpRequest = new ServletServerHttpRequest(request);
+		
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			//fazendo com que as propriedades ignoradas no json sejam 'barradas'
+			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+			//fazendo com que as propriedades não existentes no json sejam 'barradas'
+			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+		
+			Restaurante restauranteOrigem = objectMapper.convertValue(camposOrigem, Restaurante.class);
+		
+			camposOrigem.forEach((nomeProp, valorProp) ->{
+				Field field = ReflectionUtils.findField(Restaurante.class, nomeProp);
+				field.setAccessible(true); //muda tipo de acesso das propriedades- private para public
 			
-			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem); //necessário para que tipo de lista seja convetido para o mesmo tipo que está na classe
-			ReflectionUtils.setField(field, restauranteDestino, novoValor);
-		});
-	}*/
+				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem); //necessário para que tipo de lista seja convetido para o mesmo tipo que está na classe
+				ReflectionUtils.setField(field, restauranteDestino, novoValor);
+			});
+		} catch (IllegalArgumentException ex) {
+			//aqui vou forçar pegar um tipo de exceção, e em seguida relançar como um tipo tratado
+			//pegando a causa da exceção 
+			Throwable rootCause = ExceptionUtils.getRootCause(ex);
+			
+			throw new HttpMessageNotReadableException(ex.getMessage(), rootCause, servletServerHttpRequest);
+		}
+	}
 		
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
