85249230f4c6a8c6b237652399916161e98ebf44
Maiara Goldacker <maiaragoldacker@gmail.com>
2020-12-01 20:47:33 -0300

    adicionando model mapper

--- src/main/java/com/api/algafood/model/input/CozinhaInput.java
+++ src/main/java/com/api/algafood/model/input/CozinhaInput.java
@@ -1,5 +1,13 @@
 package com.api.algafood.model.input;
 
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
 public class CozinhaInput {
 
+	@NotBlank	
+	private String nome;
 }
--- src/main/java/com/api/algafood/model/input/EstadoInput.java
+++ src/main/java/com/api/algafood/model/input/EstadoInput.java
@@ -1,5 +1,14 @@
 package com.api.algafood.model.input;
 
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
 public class EstadoInput {
+	
+	@NotBlank	
+	private String nome;
 
 }
