cd316f00e34ddbc176e6eb87209de28efb9e49c3
Maiara Goldacker <maiaragoldacker@gmail.com>
2021-02-12 11:43:23 -0300

    tratando bind exception

--- src/main/java/com/api/algafood/controller/PedidoController.java
+++ src/main/java/com/api/algafood/controller/PedidoController.java
@@ -4,16 +4,13 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,8 +31,6 @@ import com.api.algafood.domain.service.CadastroUsuarioService;
 import com.api.algafood.model.PedidoModel;
 import com.api.algafood.model.PedidoResumoModel;
 import com.api.algafood.model.input.PedidoInput;
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 @RequestMapping("/pedidos")
--- src/main/java/com/api/algafood/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/api/algafood/exceptionHandler/ApiExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -299,4 +300,11 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 		    
 		    return handleExceptionInternal(ex, problem, headers, status, request);
 		}
+	
+	@Override
+	protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status,
+			WebRequest request) {
+	
+		return handleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
+	}
 }
