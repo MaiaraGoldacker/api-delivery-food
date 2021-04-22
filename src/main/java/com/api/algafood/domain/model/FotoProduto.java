11e3b22b3c35b37585e37959775122eff5aed786
Maiara Goldacker <maiaragoldacker@gmail.com>
2021-04-22 09:08:15 -0300

    Criando classe e mapeamento foto_produto

---
+++ src/main/java/com/api/algafood/domain/model/FotoProduto.java
@@ -0,0 +1,36 @@
+package com.api.algafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+
+import com.api.algafood.validation.Groups;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class FotoProduto {
+
+	@NotNull(groups =  Groups.EstadoId.class)
+	@EqualsAndHashCode.Include
+	@Id
+	@Column(name = "produto_id")
+	private Long id;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@MapsId // é mapeada através do id da foto_produto - usando o mesmo id_produto pra mapear o Long id e o Produto produto
+	private Produto produto;
+	
+	private String nomeArquivo;
+	private String descricao;
+	private String contentType;
+	private Long tamanho;
+	
+}
--- src/main/java/com/api/algafood/model/input/FotoProdutoInput.java
+++ src/main/java/com/api/algafood/model/input/FotoProdutoInput.java
@@ -3,8 +3,10 @@ package com.api.algafood.model.input;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.api.algafood.validation.FileContentType;
 import com.api.algafood.validation.FileSize;
 
 import lombok.Getter;
@@ -14,7 +16,8 @@ import lombok.Setter;
 public class FotoProdutoInput {
 
 	@NotNull
-	@FileSize(max = "20KB")
+	@FileSize(max = "500KB")
+	@FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
 	private MultipartFile arquivo;
 	
 	@NotBlank
