e7c37b0eda0d6c18bdd57d69196a8fa44b9a2e48
Maiara Goldacker <maiaragoldacker@gmail.com>
2021-06-03 13:22:06 -0300

    inserindo foto em disco local

--- src/main/java/com/api/algafood/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/api/algafood/controller/RestauranteProdutoFotoController.java
@@ -1,5 +1,7 @@
 package com.api.algafood.controller;
 
+import java.io.IOException;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +35,7 @@ public class RestauranteProdutoFotoController {
 	private FotoProdutoModelAssembler fotoProdutoModelAssembler;
 	
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId,@PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) {
+	public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId,@PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) throws IOException {
 
 		Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
 		MultipartFile arquivo = fotoProdutoInput.getArquivo();
@@ -45,7 +47,7 @@ public class RestauranteProdutoFotoController {
 		foto.setTamanho(arquivo.getSize());
 		foto.setNomeArquivo(arquivo.getOriginalFilename());
 		
-		FotoProduto fotoSalva = catalogoFotoProdutoService.salvar(foto);
+		FotoProduto fotoSalva = catalogoFotoProdutoService.salvar(foto, arquivo.getInputStream());
 
 		return fotoProdutoModelAssembler.toModel(fotoSalva);
 	}
--- src/main/java/com/api/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/api/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -1,5 +1,6 @@
 package com.api.algafood.domain.service;
 
+import java.io.InputStream;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.api.algafood.domain.model.FotoProduto;
 import com.api.algafood.domain.repository.ProdutoRepository;
+import com.api.algafood.domain.service.FotoStorageService.NovaFoto;
 
 @Service
 public class CatalogoFotoProdutoService {
@@ -15,10 +17,14 @@ public class CatalogoFotoProdutoService {
 	@Autowired 
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private FotoStorageService fotoStorageService;
+	
 	@Transactional
-	public FotoProduto salvar(FotoProduto foto) {
+	public FotoProduto salvar(FotoProduto foto, InputStream dadosArquivo) {
 		Long restauranteId = foto.getRestauranteId();
 		Long produtoId = foto.getProduto().getId();
+		String nomeNovoArquivo = fotoStorageService.gerarNomeArquivo(foto.getNomeArquivo());
 		
 		Optional<FotoProduto> fotoExistente = produtoRepository.findFotoById(restauranteId, produtoId);
 		
@@ -26,7 +32,19 @@ public class CatalogoFotoProdutoService {
 			produtoRepository.delete(fotoExistente.get());
 		}
 		
-		return produtoRepository.save(foto);
+		//primeiro salva, pois pode dar erro de banco ao salvar. Se salvar por ultimo, a foto vai estar no disco local, porém não vai estar salva em BD, e isso é um problema.
+		foto.setNomeArquivo(nomeNovoArquivo);
+		foto = produtoRepository.save(foto);
+		produtoRepository.flush(); //Forçar a descarga de  qualquer coisa que estiver em fila
+		
+		NovaFoto novaFoto = NovaFoto.builder()
+				.nomeArquivo(foto.getNomeArquivo())
+				.inputStream(dadosArquivo)
+				.build();
+		
+		fotoStorageService.armazenar(novaFoto);
+		
+		return foto;
 	}
 	
 }
